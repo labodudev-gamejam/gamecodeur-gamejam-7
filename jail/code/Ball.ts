292d7bda719e95365efddb5c17a5293d695caf48
Jimmy <jimmy@eoxia.com>
2017-05-20 12:35:04 +0200

    Prototype: Ball

---
+++ jail/code/Ball.ts
@@ -0,0 +1,34 @@
+/**
+Créer par Jimmy Latour, 2017
+http://jimmylatour.com
+*/
+
+class Ball {
+
+  /**
+  * Le constructeur initialise les collisions du personnage
+  * @return {void}
+  */
+  constructor(public pos: any) {}
+  /**
+  *
+  */
+  public Update():void {
+  }
+
+  /**
+  * Déplaces le canvas pour le centrer sur le personnage.
+  * Fait une rotation selon son angle.
+  * Appelle la méthode Draw de tous ses enfants.
+  * @param {any} context [description]
+  */
+  public Draw(context: any):void {
+    
+  }
+
+  /**
+  * Nettoies toutes les variables
+  */
+  public Clear():void {
+  }
+}
---
+++ jail/js/code/Ball.js
@@ -0,0 +1,13 @@
+var Ball = (function () {
+    function Ball(pos) {
+        this.pos = pos;
+    }
+    Ball.prototype.Update = function () {
+    };
+    Ball.prototype.Draw = function (context) {
+    };
+    Ball.prototype.Clear = function () {
+    };
+    return Ball;
+}());
+//# sourceMappingURL=Ball.js.map
